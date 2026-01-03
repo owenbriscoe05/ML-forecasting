58cee21ff7dce584e3b41a9f608bdaf4760a94b7
Owen Briscoe <146141505+owenbriscoe05@users.noreply.github.com>
2026-01-03 01:03:41 -0500

    rewrite collection scripts to fix typo in lat/lo n box. begin processing functions.

--- src/data/collect.py
+++ src/data/collect.py
@@ -7,58 +7,93 @@ from calendar import monthrange
 YEARS = range(2000, 2025)
 HOURS = [f"{i:02d}:00" for i in range(24)]
 # Lat and lon bounding box for central PA
-AREA = [41.50, 40.00, -78.50, -76.50]
+AREA = [41.50, -78.50, 40.0, -76.50]
 
 GHCN_STATION_METADATA_URL = "https://www.ncei.noaa.gov/pub/data/ghcn/daily/ghcnd-stations.txt"
 GHCN_BASE_URL = "https://www.ncei.noaa.gov/pub/data/ghcn/daily/by_station/"
 GHCN_DIR = "data/raw/ghcn"
 
-# no explicit retrieve function for ghcn data
-os.makedirs(GHCN_DIR, exist_ok=True)
-
 c = cdsapi.Client()
+
+def download_era5_only():
+    # download_era5_data(dataset="reanalysis-era5-single-levels", variables = [
+    #    "2m_temperature", "surface_pressure", "10m_u_component_of_wind", "10m_v_component_of_wind",
+    #    "total_precipitation", "2m_dewpoint_temperature", "total_column_water_vapour",
+    #    "precipitation_type", "cape",
+    #    "convective_inhibition", "boundary_layer_height", "surface_solar_radiation_downwards"]) 
+    
+
+    download_era5_data(dataset="reanalysis-era5-pressure-levels", variables = [
+        "geopotential", "temperature", "u_component_of_wind", "v_component_of_wind",
+        "vertical_velocity", "specific_humidity"], pressure_levels=["850", "700", "500", "300"]) 
+
+def download_everything():
+    # no explicit retrieve function for ghcn data
+    os.makedirs(GHCN_DIR, exist_ok=True)
+
+    stations = get_ghcn_stations(AREA)
+    print(f"found {len(stations)} stations in this bounding box")
+    for station in stations["id"].values:
+        download_ghcn_data(station)
+
+    download_era5_data(dataset="reanalysis-era5-single-levels", variables = [
+        "2m_temperature", "surface_pressure", "10m_u_component_of_wind", "10m_v_component_of_wind",
+        "total_precipitation", "2m_dewpoint_temperature", "total_column_water_vapour",
+        "precipitation_type", "cape",
+        "convective_inhibition", "boundary_layer_height", "surface_solar_radiation_downwards"]) 
+    
+
+    download_era5_data(dataset="reanalysis-era5-pressure-levels", variables = [
+        "geopotential", "temperature", "u_component_of_wind", "v_component_of_wind",
+        "vertical_velocity", "specific_humidity"], pressure_levels=["850", "700", "500", "300"]) 
+   
 
 def download_era5_data(dataset, variables, pressure_levels=None):
     """loops through years and months, collecting era5 data"""
-    for year in YEARS:
-        for month in range (1, 13):
-            # handles number of days for different months, monthrange also handles leap years
-            _, numdays = monthrange(year, month)
-            days = [f"{1:02d}" for i in range(1, numdays + 1)]
+    for var in variables:
+        for year in YEARS:
+            for month in range (1, 13):
+                # handles number of days for different months, monthrange also handles leap years
+                _, numdays = monthrange(year, month)
+                days = [f"{i:02d}" for i in range(1, numdays + 1)]
 
-            # naming convention for raw data files for this project
-            target_name = f"data/raw/{dataset.split('_')[-1]}_{year}-{month:02d}.nc"
-            
-            # check existence (downloads can take multiple days)
-            if os.path.exists(target_name):
-                continue
+                # naming convention for raw data files for this project
+                var_clean = var.replace("/", "_")
+                target_name = f"data/raw/{dataset.split('_')[-1]}_{year}-{month:02d}_{var_clean}.nc"
+                
+                # check existence (downloads can take multiple days)
+                if os.path.exists(target_name):
+                    continue
 
-            # define the request dictionary
-            request = {
-                "product_type": ["reanalysis"],
-                "data_format": "netcdf",
-                # "download_format": "unarchived",
-                "variable": variables,
-                "year": str(year),
-                "month": f"{month:02d}",
-                "day": days,
-                "time": HOURS,
-                "area": AREA,
-            }
+                # define the request dictionary
+                request = {
+                    "product_type": ["reanalysis"],
+                    "data_format": "netcdf",
+                    # "download_format": "unarchived",
+                    "variable": [var],
+                    "year": str(year),
+                    "month": f"{month:02d}",
+                    "day": days,
+                    "time": HOURS,
+                    "area": AREA,
+                }
 
-            if pressure_levels:
-                request["pressure_level"] = pressure_levels
-            
-            print(f"Requesting: {target_name}...")
+                if pressure_levels:
+                    request["pressure_level"] = pressure_levels
+                
+                print(f"Requesting: {var} for {target_name}...")
 
-            c.retrieve(dataset, request, target_name)
+                try:
+                    c.retrieve(dataset, request, target_name)
+                except Exception as e:
+                    print(f"Failed {target_name}: {e}")
 
 def get_ghcn_stations(area):
     """parses GHCN metadata to find stations within bounds"""
     df = pd.read_fwf(GHCN_STATION_METADATA_URL, header=None,
                      widths=[11, 9, 10, 7, 3, 31],
                      names=["id", "lat", "lon", "elev", "state", "name"])
-    north, south, west, east, = area
+    north, west, south, east = area
     df["lat"] = pd.to_numeric(df["lat"], errors="coerce")
     print(f"lat: {df['lat']}")
     df["lon"] = pd.to_numeric(df["lon"], errors="coerce")
@@ -85,19 +120,7 @@ def download_ghcn_data(station_id):
         print(f"Failed to download {station_id}: {e}")
 
 
-download_era5_data(dataset="reanalysis-era5-single-levels", variables = [
-    "2m_temperature", "surface_pressure", "10m_u_component_of_wind", "10m_v_component_of_wind",
-    "total_precipitation", "2m_dewpoint_temperature", "total_column_water_vapour",
-    "precipitation_type", "cape",
-    "convective_inhibition", "boundary_layer_height", "surface_solar_radiation_downwards"]) 
-    
-
-download_era5_data(dataset="reanalysis-era5-pressure-levels", variables = [
-    "geopotential", "temperature", "u_component_of_wind", "v_component_of_wind",
-    "vertical_velocity", "specific_humidity"], pressure_levels=["850", "700", "500", "300"]) 
-    
-
-stations = get_ghcn_stations(AREA)
-print(f"found {len(stations)} stations in this bounding box")
-for station in stations["id"].values:
-    download_ghcn_data(station)
+if __name__ == "__main__":
+    if os.listdir("data/raw/ghcn/"):
+        download_era5_only()
+    else: download_everything()
--- src/data/processing.py
+++ src/data/processing.py
@@ -1,1 +1,160 @@
 import pandas as pd
+import xarray as xr
+import os
+import glob
+import zipfile
+import shutil
+from pathlib import Path
+from collect import get_ghcn_stations, AREA
+
+directory_path = Path("./data/raw/ghcn/")
+era5_path = Path("./data/raw/era5/")
+START_YEAR = 2000
+END_YEAR = 2025
+
+def rank_stations(start_year, end_year):
+    results = []
+
+    for file in directory_path.iterdir():
+        df = pd.read_csv(file, header=None, names=["id", "date", "element", "value", "mflag", "qflag", "sflag", "obs_time"])
+        # print(f"successful read on file {file}")
+        # print(df)
+
+        df["date"] = pd.to_datetime(df["date"], format="%Y%m%d")
+
+        mask = (df["date"].dt.year >= start_year) & (df["date"].dt.year <= end_year)
+        subset = df[mask]
+
+        if subset.empty:
+            continue
+
+        tmax_count = len(subset[subset["element"] == "TMAX"])
+        tmin_count = len(subset[subset["element"] == "TMIN"])
+        precip_count = len(subset[subset["element"] == "PRCP"])
+        wind_count = len(subset[subset["element"] == "AWND"])
+
+        # create dictionary of counts
+        results.append({
+            "station_id": os.path.basename(file).replace(".csv.gz", ""),
+            "tmax_days": tmax_count,
+            "tmin_days": tmin_count,
+            "precip_days": precip_count,
+            "wind_days": wind_count,
+            # need enough data to accurately verify predictions
+            "good_coverage": bool((tmax_count > 9000) & (tmin_count > 9000) & (wind_count > 9000))
+        })
+
+    print(results)
+    ranking = pd.DataFrame(results).sort_values(by="good_coverage", ascending = False)
+    # print(ranking)
+    # print(len(ranking["good_coverage" == True]))
+    return ranking
+
+def clean_selected_data(stations):
+    cleaned = []
+    for s in stations.itertuples():
+        name = s.station_id
+        df = pd.read_csv(f"{directory_path}/{name}.csv.gz", header=None, names=["id", "date", "element", "value", "mflag", "qflag", "sflag", "obs_time"])
+        df["date"] = pd.to_datetime(df["date"], format="%Y%m%d")
+
+        mask = (df["date"].dt.year >= START_YEAR) & (df["date"].dt.year <= END_YEAR)
+        subset = df[mask]
+
+        # raw data is not in decimal point form
+        subset["value"] = subset["value"] / 10
+        subset = subset[subset["value"] != "NaN"]
+
+        # print(subset)
+
+        cleaned.append(subset)
+
+    return cleaned
+
+def unzip_era5():
+    """era5 files were downloaded as zips and need to be unpacked"""
+    zips = glob.glob(os.path.join(era5_path, "*.nc"))
+    for f in zips:
+        if zipfile.is_zipfile(f):
+            with zipfile.ZipFile(f, 'r') as zip_ref:
+                extract_to = f.replace(".nc", "_extracted")
+                zip_ref.extractall(extract_to)
+
+def collect_era5():
+    file_pattern = os.path.join(era5_path, "**/*.nc")
+    ds = xr.open_mfdataset(
+        file_pattern,
+        combine="by_coords",
+        chunks={"time": 24},
+        engine="netcdf4",
+        data_vars="minimal",
+        coords="minimal",
+        compat="override"
+    )
+        
+    
+    return ds
+
+
+
+
+
+def merge_with_era5(ghcn, era5, lat, lon):
+    merged = pd.DataFrame
+    point_ds = era5.sel(latitude=lat, longitude=lon, method="nearest").compute()
+
+    if "valid_time" in point_ds.coords:
+        point_ds = point_ds.rename({"valid_time": "time"})
+
+    daily_era5 = xr.Dataset()
+    daily_era5["era5_temp_mean"] = point_ds["t2m"].resample(time="1D").mean() - 273.15
+    daily_era5["era5_precip_sum"] = point_ds["tp"].resample(time="1D").sum() * 1000
+    # daily_era5["era5_precip_type"] = point_ds["ptype"].resample(time="1D").
+
+    era5_df = daily_era5.to_dataframe().reset_index()
+    era5_df = era5_df.rename(columns={"time": "date"})
+
+    ghcn["date"] = pd.to_datetime(ghcn["date"])
+    era5_df["date"] = pd.to_datetime(era5_df["date"])
+
+    merged = pd.merge(ghcn, era5_df, on="date", how="inner")
+
+    return merged
+        
+
+def build_features(df):
+    ...
+
+
+ranking = rank_stations(START_YEAR, END_YEAR)
+selected = ranking.head(5)
+# unzip_era5()
+xr_era5 = collect_era5()
+print(xr_era5)
+print("save complete")
+
+metadata_df = get_ghcn_stations(AREA)
+selected_with_coords = pd.merge(selected, metadata_df[["id", "lat", "lon"]],
+                               left_on="station_id", right_on="id")
+
+for station in selected_with_coords.itertuples():
+    # Load and clean ONLY this station
+    df_ghcn = pd.read_csv(f"{directory_path}/{station.station_id}.csv.gz", 
+                          header=None, 
+                          names=["id", "date", "element", "value", "mflag", "qflag", "sflag", "obs_time"])
+    
+    # Clean ghcn data (i.e. datetime and unit conversion)
+    df_ghcn["date"] = pd.to_datetime(df_ghcn["date"], format="%Y%m%d")
+    df_ghcn = df_ghcn[(df_ghcn["date"].dt.year >= START_YEAR) & (df_ghcn["date"].dt.year <= END_YEAR)]
+    df_ghcn["value"] = df_ghcn["value"] / 10
+
+    # Merge with ERA5 using the coordinates from 'selected_with_coords'
+    merged_data = merge_with_era5(df_ghcn, xr_era5, station.lat, station.lon)
+    
+    # Save this specific station's training file
+    # merged_data.to_csv(f"data/processed/{station.station_id}_merged.csv", index=False)
+    print(merged_data)
+    merged_data.to_csv("~/Downloads/check_data.csv")
+    break
+# merged_data = merge_with_era5(cleaned, xr_era5)
+
+
